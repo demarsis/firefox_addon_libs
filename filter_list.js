05f5c86219d68ddd527ae3962ad06c7c2146a69d
demarsis <demarsis@mail.ru>
2017-12-06 17:08:16 +0200

    First commit.

---
+++ filter_list.js
@@ -0,0 +1,16 @@
+new ResponceFiler(
+	["https://2.cdn.echo.msk.ru/assets/application-*.js"],
+	["script"],
+	[
+		{
+			target  : 'c.find(".toright").on("click",function(e){',
+			replace : 'c.find(".toright").on("click",function(e){alert("Right!");return;'
+		},
+		{
+			target  : 'c.find(".toleft").on("click",function(e){',
+			replace : 'c.find(".toleft").on("click",function(e){alert("Left!");return;'
+		}
+	]
+);
+
+
