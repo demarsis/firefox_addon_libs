daa2981113b54cc0c4c945c653c38819b2ef7657
demarsis <demarsis@mail.ru>
2019-12-13 21:55:36 +0200

    Added castle2 branch (works for any url)

--- background.js
+++ background.js
@@ -1,15 +1,11 @@
 // Can set rating up/down as a joke (this doesn't vote for real)
 new ResponceFilter(
-	["https://cs.pikabu.ru/apps/desktop/1.32.12/main/app*js"],
+	["https://*/*game.js?*"],
 	["script"],
 	[
 		{
-			target  : 'this.initParams.userID>0',
-			replace : 'true'
-		},
-		{
-			target  : 't=this.vote+t.valueOf(),',
-			replace : 't=this.vote+t.valueOf(),alert(t),'			
+			target  : 'isFixedLines=function(){return!e}',
+			replace : 'isFixedLines=function(){return 0}'
 		}
 	]
 );
